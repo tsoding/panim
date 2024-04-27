2ecb211e5529a31c8c7bdc417116e7879d7c659a
rexim <reximkut@gmail.com>
2024-04-28 04:42:05 +0700

    First draft of the animation

--- plug.c
+++ plug.c
@@ -11,7 +11,6 @@
 typedef struct {
     size_t i;
     float duration;
-    bool loop;
 } Animation;
 
 typedef struct {
@@ -27,11 +26,6 @@ void plug_init(void)
     assert(p != NULL);
     memset(p, 0, sizeof(*p));
     p->background = GREEN;
-    p->a.loop = false;
-
-    TraceLog(LOG_INFO, "------------------------------");
-    TraceLog(LOG_INFO, "Initialized plugin");
-    TraceLog(LOG_INFO, "------------------------------");
 }
 
 void *plug_pre_reload(void)
@@ -53,72 +47,55 @@ typedef struct {
 float animation_value(Animation a, Keyframe *kfs, size_t kfs_count)
 {
     assert(kfs_count > 0);
-
-    if (a.i >= kfs_count) {
-        return kfs[kfs_count - 1].to;
-    }
-
-    Keyframe *kf = &kfs[a.i];
-
-    return Lerp(kf->from, kf->to, a.duration/kf->duration);
+    Keyframe *kf = &kfs[a.i%kfs_count];
+    float t = a.duration/kf->duration;
+    t = (sinf(PI*t - PI*0.5) + 1)*0.5;
+    return Lerp(kf->from, kf->to, t);
 }
 
 void animation_update(Animation *a, Keyframe *kfs, size_t kfs_count)
 {
     assert(kfs_count > 0);
 
-    if (a->i >= kfs_count) {
-        if (a->loop) {
-            a->i = 0;
-            a->duration = 0.0;
-        } else {
-            return;
-        }
-    }
+    a->i = a->i % kfs_count;
+    a->duration += GetFrameTime();
 
-    float dt = GetFrameTime();
-    a->duration += dt;
-
-    while (a->i < kfs_count && a->duration >= kfs[a->i].duration) {
+    while (a->duration >= kfs[a->i].duration) {
         a->duration -= kfs[a->i].duration;
-        a->i += 1;
+        a->i = (a->i + 1) % kfs_count;
     }
 }
 
 void plug_update(void)
 {
-    Keyframe kfs[] = {
-        {
-            .from = 0.0,
-            .to = 0.250,
-            .duration = 0.250,
-        },
-        {
-            .from = 0.25,
-            .to = 0.25,
-            .duration = 0.5,
-        },
-        {
-            .from = 0.25,
-            .to = 1.0,
-            .duration = 0.5,
-        }
-    };
-
+    float rw = 100.0;
+    float rh = 100.0;
+    float pad = rw*0.15;
     float w = GetScreenWidth();
     float h = GetScreenHeight();
+
+    size_t offset = 7;
+    Keyframe kfs[] = {
+        {.from = w/2 - rw/2 - (offset + 0)*(rw + pad), .to = w/2 - rw/2 - (offset + 0)*(rw + pad), .duration = 0.5,},
+        {.from = w/2 - rw/2 - (offset + 0)*(rw + pad), .to = w/2 - rw/2 - (offset + 1)*(rw + pad), .duration = 0.5,},
+        {.from = w/2 - rw/2 - (offset + 1)*(rw + pad), .to = w/2 - rw/2 - (offset + 1)*(rw + pad), .duration = 0.5,},
+        {.from = w/2 - rw/2 - (offset + 1)*(rw + pad), .to = w/2 - rw/2 - (offset + 2)*(rw + pad), .duration = 0.5,},
+        {.from = w/2 - rw/2 - (offset + 2)*(rw + pad), .to = w/2 - rw/2 - (offset + 2)*(rw + pad), .duration = 0.5,},
+        {.from = w/2 - rw/2 - (offset + 2)*(rw + pad), .to = w/2 - rw/2 - (offset + 3)*(rw + pad), .duration = 0.5,},
+        {.from = w/2 - rw/2 - (offset + 3)*(rw + pad), .to = w/2 - rw/2 - (offset + 3)*(rw + pad), .duration = 0.5,},
+        {.from = w/2 - rw/2 - (offset + 3)*(rw + pad), .to = w/2 - rw/2 - (offset + 0)*(rw + pad), .duration = 0.5,},
+    };
+
+    Color cell_color = ColorFromHSV(0, 0.0, 0.15);
+    Color head_color = ColorFromHSV(200, 0.8, 0.8);
+    Color background_color = ColorFromHSV(120, 0.0, 1);
 
     BeginDrawing();
     animation_update(&p->a, kfs, NOB_ARRAY_LEN(kfs));
     float t = animation_value(p->a, kfs, NOB_ARRAY_LEN(kfs));
-    ClearBackground(GetColor(0x181818FF));
-    float rw = 100.0;
-    float rh = 100.0;
-    float pad = rw*0.15;
-    Color cell_color = ColorFromHSV(0, 0.8, 1);
+    ClearBackground(background_color);
     for (size_t i = 0; i < 20; ++i) {
-        DrawRectangle(i*(rw + pad) - w*t, h/2 - rh/2, rw, rh, cell_color);
-        // DrawRectangle(i*(rw + pad) + GetMousePosition().x - w, h/2 - rh/2, rw, rh, cell_color);
+        DrawRectangle(i*(rw + pad) + t, h/2 - rh/2, rw, rh, cell_color);
     }
 
     float head_thick = 20.0;
@@ -128,7 +105,7 @@ void plug_update(void)
     };
     rec.x = w/2 - rec.width/2;
     rec.y = h/2 - rec.height/2;
-    DrawRectangleLinesEx(rec, head_thick, YELLOW);
+    DrawRectangleLinesEx(rec, head_thick, head_color);
 
     EndDrawing();
 }
